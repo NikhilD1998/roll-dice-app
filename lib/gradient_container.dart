90b0a0d60ec97873212e936a4a3e82f9a400d8c5
NikhilD1998 <nikhild.dev.1998@gmail.com>
2023-05-05 12:30:05 +0530

    Styled Components

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -17,10 +18,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(color: Colors.red, fontSize: 16),
-        ),
+        child: StyledText(),
       ),
     );
   }
